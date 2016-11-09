a75cb30d2e3de4e5ebf3f64f0e7f28c26c6914f2
kendrarepo <kjrepo@umich.edu>
2016-11-08 21:12:11 -0500

    Changes to Twitter3a

--- kjrepoHW3/twitterhw3a.py
+++ kjrepoHW3/twitterhw3a.py
@@ -4,5 +4,20 @@
 
 # You will demo this live for grading.
 
-print("""No output necessary although you 
-	can print out a success/failure message if you want to.""")
+# print("""No output necessary although you can print out a success/failure message if you want to.""")
+
+import tweepy 
+
+# Unique code from Twitter
+access_token = "273601600-wyF84WnBs29PZCuLR72Jd9JCydGwpRCTlE3dECcU"
+access_token_secret = "5gkkFlPFdtKbnHsVIxTfnbSwzoPa2bnIVbNg2Dr9yrpEF"
+consumer_key = "LDHNJnUom7eodTDkw19kt34H3"
+consumer_secret = "bnaVcIwvy1XE1ZnlFBrBiip7Gy1zR8wgtfNtvctt1nAEXdLdEW"
+
+# Boilerplate code here
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth) # Now we can Create Tweets, Delete Tweets, and Find Twitter Users
+
+proj3status = api.update_with_media(filename = "umsi.jpeg", status = 'This post is for a project in my programming class #UMSI206 #Proj3')
