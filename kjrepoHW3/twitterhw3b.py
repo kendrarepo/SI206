2ab884c67512c50ce9b1aa48d97f87f8d88e97b5
kendrarepo <kjrepo@umich.edu>
2016-11-09 14:52:29 -0500

    Done with twitterhw3b.py

--- kjrepoHW3/twitterhw3b.py
+++ kjrepoHW3/twitterhw3b.py
@@ -6,7 +6,36 @@
 # 3) Print the average polarity of the results
 
 # Be prepared to change the search term during demo.
+import tweepy
+from textblob import TextBlob
 
+# Unique code from Twitter
+access_token = "273601600-wyF84WnBs29PZCuLR72Jd9JCydGwpRCTlE3dECcU"
+access_token_secret = "5gkkFlPFdtKbnHsVIxTfnbSwzoPa2bnIVbNg2Dr9yrpEF"
+consumer_key = "LDHNJnUom7eodTDkw19kt34H3"
+consumer_secret = "bnaVcIwvy1XE1ZnlFBrBiip7Gy1zR8wgtfNtvctt1nAEXdLdEW"
 
-print("Average subjectivity is")
-print("Average polarity is")
+# Boilerplate code here
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.search('UMSI')
+
+results = 0
+subjectivity = 0.0
+polarity = 0.0
+
+for tweet in public_tweets:
+	results += 1
+	print(tweet.text)
+	analysis = TextBlob(tweet.text)
+	subjectivity += analysis.subjectivity
+	polarity += analysis.polarity
+
+ave_subjectivity = subjectivity/results
+ave_polarity = polarity/results
+
+print("Average subjectivity is: " + str(ave_subjectivity))
+print("Average polarity is: " + str(ave_polarity))
